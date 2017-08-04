6521433b85c66c6f4bad35d26509a1eaba869509
Ruben Mattan <ruben@mobicage.com>
2017-08-04 15:01:59 +0200

    Add bizz methods for iyo keystore

--- plugins/tff_backend/bizz/iyo_see.py
+++ plugins/tff_backend/bizz/iyo_see.py
@@ -19,63 +19,59 @@ import httplib
 import json
 import logging
 
+from mcfw.exceptions import HttpException
+
 from framework.bizz.authentication import get_current_session
-from mcfw.rpc import returns, arguments
 from plugins.its_you_online_auth.bizz.authentication import get_itsyouonline_client_from_jwt
 from plugins.tff_backend.to.iyo.see import IYOSeeDocument, IYOSeeDocumentView
-from plugins.tff_backend.utils import raise_http_exception
 
 
-@returns(IYOSeeDocument)
-@arguments(organization_id=unicode, username=unicode, uniqueid=unicode)
+def _raise_http_exception(status_code, result_text):
+    try:
+        err = json.loads(result_text)
+        e = HttpException(err.get('error'), err)
+    except ValueError:
+        e = HttpException(result_text)
+    e.http_code = status_code
+    raise e
+
 def get_see_document(organization_id, username, uniqueid):
     jwt = get_current_session().jwt
     result = get_itsyouonline_client_from_jwt(jwt).api.users.GetSeeObject(uniqueid, username, organization_id)
     logging.debug('get_see_document %s %s', result.status_code, result.text)
     if result.status_code != httplib.OK:
-        raise_http_exception(result.status_code, result.text)
+        _raise_http_exception(result.status_code, result.text)
     return IYOSeeDocument(**result)
 
 
-@returns(IYOSeeDocumentView)
-@arguments(organization_id=unicode, username=unicode)
 def get_see_documents(organization_id, username):
     jwt = get_current_session().jwt
     result = get_itsyouonline_client_from_jwt(jwt).api.users.ListSeeObjectsByOrganization(username, organization_id)
     logging.debug('get_see_documents %s %s', result.status_code, result.text)
     if result.status_code != httplib.OK:
-        raise_http_exception(result.status_code, result.text)
+        _raise_http_exception(result.status_code, result.text)
     return [IYOSeeDocumentView(**d) for d in result.json()]
 
 
-@returns(IYOSeeDocumentView)
-@arguments(organization_id=unicode, username=unicode, data=IYOSeeDocumentView)
 def create_see_document(organization_id, username, data):
     jwt = get_current_session().jwt
     result = get_itsyouonline_client_from_jwt(jwt).api.users.CreateSeeObject(data, username, organization_id)
     logging.debug('create_see_document %s %s', result.status_code, result.text)
     if result.status_code not in (httplib.CREATED, httplib.CONFLICT):
-        raise_http_exception(result.status_code, result.text)
-    return IYOSeeDocumentView(**result.json())
+        _raise_http_exception(result.status_code, result.text)
 
 
-@returns(IYOSeeDocumentView)
-@arguments(organization_id=unicode, username=unicode, data=IYOSeeDocumentView)
-def update_see_document(organization_id, username, data):
+def update_see_document( organization_id, username, data):
     jwt = get_current_session().jwt
     result = get_itsyouonline_client_from_jwt(jwt).api.users.UpdateSeeObject(data, data.uniqueid, username, organization_id)
     logging.debug('update_see_document %s %s', result.status_code, result.text)
     if result.status_code not in (httplib.CREATED,):
-        raise_http_exception(result.status_code, result.text)
-    return IYOSeeDocumentView(**result.json())
+        _raise_http_exception(result.status_code, result.text)
 
 
-@returns(IYOSeeDocumentView)
-@arguments(organization_id=unicode, username=unicode, data=IYOSeeDocumentView)
 def sign_see_document(organization_id, username, data):
     jwt = get_current_session().jwt
     result = get_itsyouonline_client_from_jwt(jwt).api.users.SignSeeObject(data, data.version, data.uniqueid, username, organization_id)
     logging.debug('sign_see_document %s %s', result.status_code, result.text)
     if result.status_code not in (httplib.CREATED,):
-        raise_http_exception(result.status_code, result.text)
-    return IYOSeeDocumentView(**result.json())
+        _raise_http_exception(result.status_code, result.text)
--- plugins/tff_backend/utils/__init__.py
+++ plugins/tff_backend/utils/__init__.py
@@ -19,10 +19,10 @@ import json
 import re
 
 from mcfw.rpc import returns, arguments
+from mcfw.exceptions import HttpException
 from plugins.rogerthat_api.to import UserDetailsTO
 from plugins.rogerthat_api.to.messaging.flow import BaseFlowStepTO
 from plugins.rogerthat_api.to.messaging.forms import FormResultTO
-
 
 HUMAN_READABLE_TAG_REGEX = re.compile('(.*?)\\s*\\{.*\\}')
 
@@ -61,3 +61,13 @@ def get_step(steps, step_id):
 def get_step_value(steps, step_id):
     step = get_step(steps, step_id)
     return step and step.get_value()
+
+
+def raise_http_exception(status_code, result_text):
+    try:
+        err = json.loads(result_text)
+        e = HttpException(err.get('error'), err)
+    except ValueError:
+        e = HttpException(result_text)
+    e.http_code = status_code
+    raise e
