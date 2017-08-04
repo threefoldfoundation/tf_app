fb6b50faebd66730ddfd9213e18200c2b919016c
Ruben Mattan <rubenmattan@gmail.com>
2017-08-04 08:14:48 +0200

    Allow missing fields in iyo see document

--- plugins/tff_backend/to/iyo_see.py
+++ plugins/tff_backend/to/iyo_see.py
@@ -1,6 +1,14 @@
 from mcfw.properties import unicode_property, typed_property, long_property
 from plugins.tff_backend.to.to import TO
 
+def _convert_to_unicode(v):
+    if v is None:
+        return None
+    if isinstance(v, unicode):
+        return v
+    return unicode(v)
+
+
 class IYOSeeDocumenVersion(TO):
     version = long_property('4')
     category = unicode_property('5')
@@ -18,16 +26,16 @@ class IYOSeeDocumenVersion(TO):
                  content_type=None, markdown_short_description=None, markdown_full_description=None,
                  creation_date=None, start_date=None, end_date=None, keystore_label=None, signature=None, **kwargs):
         self.version = version
-        self.category = unicode(category)
-        self.link = unicode(link)
-        self.content_type = unicode(content_type)
-        self.markdown_short_description = unicode(markdown_short_description)
-        self.markdown_full_description = unicode(markdown_full_description)
-        self.creation_date = unicode(creation_date)
-        self.start_date = unicode(start_date)
-        self.end_date = unicode(end_date)
-        self.keystore_label = unicode(keystore_label)
-        self.signature = unicode(signature)
+        self.category = _convert_to_unicode(category)
+        self.link = _convert_to_unicode(link)
+        self.content_type = _convert_to_unicode(content_type)
+        self.markdown_short_description = _convert_to_unicode(markdown_short_description)
+        self.markdown_full_description = _convert_to_unicode(markdown_full_description)
+        self.creation_date = _convert_to_unicode(creation_date)
+        self.start_date = _convert_to_unicode(start_date)
+        self.end_date = _convert_to_unicode(end_date)
+        self.keystore_label = _convert_to_unicode(keystore_label)
+        self.signature = _convert_to_unicode(signature)
 
 
 class IYOSeeDocument(TO):
@@ -37,9 +45,9 @@ class IYOSeeDocument(TO):
     versions = typed_property('4', IYOSeeDocumenVersion, True)
 
     def __init__(self, username=None, globalid=None, uniqueid=None, versions=None, **kwargs):
-        self.username = unicode(username)
-        self.globalid = unicode(globalid)
-        self.uniqueid = unicode(uniqueid)
+        self.username = _convert_to_unicode(username)
+        self.globalid = _convert_to_unicode(globalid)
+        self.uniqueid = _convert_to_unicode(uniqueid)
         if not versions:
             versions = []
         self.versions = [IYOSeeDocumenVersion(**v) for v in versions]
@@ -64,17 +72,17 @@ class IYOSeeDocumentView(TO):
     def __init__(self, username=None, globalid=None, uniqueid=None, version=1, category=None, link=None,
                  content_type=None, markdown_short_description=None, markdown_full_description=None,
                  creation_date=None, start_date=None, end_date=None, keystore_label=None, signature=None, **kwargs):
-        self.username = unicode(username)
-        self.globalid = unicode(globalid)
-        self.uniqueid = unicode(uniqueid)
+        self.username = _convert_to_unicode(username)
+        self.globalid = _convert_to_unicode(globalid)
+        self.uniqueid = _convert_to_unicode(uniqueid)
         self.version = version
-        self.category = unicode(category)
-        self.link = unicode(link)
-        self.content_type = unicode(content_type)
-        self.markdown_short_description = unicode(markdown_short_description)
-        self.markdown_full_description =unicode( markdown_full_description)
-        self.creation_date = unicode(creation_date)
-        self.start_date = unicode(start_date)
-        self.end_date = unicode(end_date)
-        self.keystore_label = unicode(keystore_label)
-        self.signature = unicode(signature)
+        self.category = _convert_to_unicode(category)
+        self.link = _convert_to_unicode(link)
+        self.content_type = _convert_to_unicode(content_type)
+        self.markdown_short_description = _convert_to_unicode(markdown_short_description)
+        self.markdown_full_description =_convert_to_unicode( markdown_full_description)
+        self.creation_date = _convert_to_unicode(creation_date)
+        self.start_date = _convert_to_unicode(start_date)
+        self.end_date = _convert_to_unicode(end_date)
+        self.keystore_label = _convert_to_unicode(keystore_label)
+        self.signature = _convert_to_unicode(signature)
