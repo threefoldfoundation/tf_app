9293ece82f5d6a74e81791f3a654ed8461a3b123
Lucas Vanhalst <lucas.vanhalst@gmail.com>
2017-09-07 15:35:47 +0200

    feat(referrals): Initial version of 'refer friend'

---
+++ brandings/refferals-invite/main.js
@@ -0,0 +1,26 @@
+(function () {
+  'use strict';
+  console.log('test');
+  var smsElem = $('#invite_sms');
+  var emailElem = $('#invite_email');
+  var codeElem = $('#invitation_code');
+  rogerthat.callbacks.ready(function () {
+    console.log('rogerthat is ready');
+    var code = rogerthat.user.data.referral_code;
+    var userName = rogerthat.user.data.name || rogerthat.user.name;
+    var appId = rogerthat.user.app_id;
+    var message = 'Hi,\n\n' +
+      'I\'d like to invite you the ThreeFold community.' +
+      ' You can download the Android or iOS app at https://rogerth.at/install?a=' + appId +
+      '\n\n' +
+      'To become a registered user in the app, you can use my invitation code ' + code + '.' +
+      '\n\n' +
+      'Welcome on board!\n' + userName;
+    var subject = 'Invitation to Threefold community';
+    var smsHref = 'sms:?body=' + encodeURIComponent(message);
+    var emailHref = 'mailto:?subject=' + encodeURIComponent(subject) + '&body=' + encodeURIComponent(message);
+    smsElem.attr('href', smsHref);
+    emailElem.attr('href', emailHref);
+    codeElem.text(code);
+  });
+})();
