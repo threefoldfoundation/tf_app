e942661644ab32fd801b999292cff6fb11c3f503
Lucas Vanhalst <lucas.vanhalst@gmail.com>
2017-09-08 17:07:20 +0200

    fix(referrals): Lowercase referral code

--- brandings/referrals/app.js
+++ brandings/referrals/app.js
@@ -1,58 +1,62 @@
-window.onload = function() {
-    console.log("window.onload");
-    $(document).ready(function() {
-        console.log("$(document).ready");
-        rogerthat.callbacks.ready(onRogerthatReady);
-    });
-}; 
+$(document).ready(function () {
+    console.log("$(document).ready");
+    rogerthat.callbacks.ready(onRogerthatReady);
+});
 
-var onRogerthatReady = function() {
+function onRogerthatReady() {
     console.log("onRogerthatReady()");
     rogerthat.api.callbacks.resultReceived(onReceivedApiResult);
 
-    $(document).on("click", ".set-referrer-button", function() {
-    	sendInvitationCode();
-    });
-};
+}
 
-var onReceivedApiResult = function(method, result, error, tag) {
+function onReceivedApiResult(method, result, error, tag) {
     console.log("onReceivedApiResult");
     console.log("method: " + method);
     console.log("result: " + result);
     console.log("error: " + error);
 
-    if (method == "referrals.set") {
-    	hideLoading();
-    	if (result) {
-    		$(".set-referrer-result").show();
+    if (method === "referrals.set") {
+        hideLoading();
+        if (result) {
+            $(".set-referrer-result").show();
             $(".set-referrer-result p").text(result);
-    	} else {
-    		$(".set-referrer-error").show();
-    		$(".set-referrer").show();
+        } else {
+            $(".set-referrer-error").show();
+            $(".set-referrer").show();
             $(".set-referrer-error p").text(error);
-    	}
+        }
     }
-};
+}
 
-var sendInvitationCode = function() {
-    $.mobile.loading( 'show', {
+function sendInvitationCode() {
+    $.mobile.loading('show', {
         text: "Validating invitation code",
         textVisible: true,
         theme: 'a',
         html: ""
     });
-    
+
     $(".set-referrer-result").hide();
-	$(".set-referrer-error").hide();
+    $(".set-referrer-error").hide();
     $(".set-referrer").hide();
-    
     var p = JSON.stringify({
-        'code' : $('#invitation_code').val()
+        'code': $('#invitation_code').val()
     });
     rogerthat.api.call("referrals.set", p, "");
-};
+}
 
-var hideLoading = function() {
+$('#invitation_code').keyup(function () {
+    var dis = $(this);
+    if (dis.val()) {
+        dis.val(dis.val().toLowerCase());
+    }
+});
+$('#set-referrer').submit(function (e) {
+    e.preventDefault();
+    sendInvitationCode();
+});
+
+function hideLoading() {
     console.log('hiding loading');
     $.mobile.loading('hide');
-};
+}
