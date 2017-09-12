63d2bdb15cf3f84948e6c32e6b23fa86d95d3c6c
Ruben Mattan <ruben@mobicage.com>
2017-09-12 11:56:52 +0200

    #76 Finished test create partner / quotation

--- plugins/tff_backend/bizz/odoo.py
+++ plugins/tff_backend/bizz/odoo.py
@@ -15,57 +15,159 @@
 #
 # @@license_version:1.3@@
 
-import erppeek
+import xmlrpclib
+import logging
+
+from google.appengine.api import urlfetch
 from framework.plugin_loader import get_config
 from plugins.tff_backend.plugin_consts import NAMESPACE
+import erppeek
+
+
+# GAEXMLRPCTransport is copied from http://brizzled.clapper.org/blog/2008/08/25/making-xmlrpc-calls-from-a-google-app-engine-application/
+class GAEXMLRPCTransport(object):
+    """Handles an HTTP transaction to an XML-RPC server."""
+
+    def __init__(self):
+        pass
+
+    def request(self, host, handler, request_body, verbose=0):
+        result = None
+        url = 'http://%s%s' % (host, handler)
+        try:
+            response = urlfetch.fetch(url,
+                                      payload=request_body,
+                                      method=urlfetch.POST,
+                                      headers={'Content-Type': 'text/xml'})
+        except:
+            msg = 'Failed to fetch %s' % url
+            logging.error(msg)
+            raise xmlrpclib.ProtocolError(host + handler, 500, msg, {})
+
+        if response.status_code != 200:
+            logging.error('%s returned status code %s' %
+                          (url, response.status_code))
+            raise xmlrpclib.ProtocolError(host + handler,
+                                          response.status_code,
+                                          "",
+                                          response.headers)
+        else:
+            result = self.__parse_response(response.content)
+
+        return result
+
+    def __parse_response(self, response_body):
+        p, u = xmlrpclib.getparser(use_datetime=False)
+        p.feed(response_body)
+        return u.close()
+    
 
 
 def get_erp_client():
     cfg = get_config(NAMESPACE)
-    return erppeek.Client(cfg.odoo.url, cfg.odoo.database, cfg.odoo.username, cfg.odoo.password)
+    return erppeek.Client(cfg.odoo.url, cfg.odoo.database, cfg.odoo.username, cfg.odoo.password, transport=GAEXMLRPCTransport(), verbose=True)
 
 
-def create_quotation(erp_client, customer):
-    odoo_customer_id = get_customer_id(erp_client, customer)
-    put_quotation_in_odoo(erp_client, odoo_customer_id)
+def update_odoo_object(odoo_object, **values):
+    odoo_object.write(values)
 
 
-def get_customer_id(erp_client, customer):
-    partner = erppeek.Model(erp_client, 'res.partner')
-    
-#     parent_id = None
-#     type = u'contact' #'u'delivery'
+def save_customer(erp_client, customer):
+    partner = erp_client.model('res.partner')
     
     odoo_contact = {
-        'parent_id': None,
         'type': u'contact',
-        'name': customer['name'],
-        'email': customer['email'],
-        'phone': customer['phone'],
+        'name': customer['billing']['name'],
+        'email': customer['billing']['email'],
+        'phone': customer['billing']['phone'],
+        'street': customer['billing']['address']
     }
     
-    if customer["odoo_contact_id"]:
-        odoo_customer = partner.browse(customer["odoo_contact_id"])
-        odoo_customer.write(odoo_contact)
+    if customer['billing']['id']:
+        odoo_partner_contact = partner.browse(customer['billing']['id'])
+        update_odoo_object(odoo_partner_contact, **odoo_contact)
     else:
-        odoo_customer = partner.create(odoo_contact)
-
-    return odoo_customer.id
-
-
-def put_quotation_in_odoo(ep_client, odoo_customer_id):
-    order = erppeek.Model(ep_client, 'sale.order')
+        odoo_partner_contact = partner.create(odoo_contact)
+        
+    if not customer['shipping']:
+        return {
+            'billing_id': odoo_partner_contact.id,
+            'shipping_id': None
+        }
     
+    odoo_delivery = {
+        'parent_id': odoo_partner_contact.id,
+        'type': u'delivery',
+        'name': customer['shipping']['name'],
+        'email': customer['shipping']['email'],
+        'phone': customer['shipping']['phone'],
+        'street': customer['shipping']['address']
+    }
+    
+    if customer['shipping']['id']:
+        odoo_partner_delivery = partner.browse(customer['shipping']['id'])
+        update_odoo_object(odoo_partner_delivery, **odoo_delivery)
+        
+    else:
+        odoo_partner_delivery = partner.create(odoo_delivery)
+        
+    return {
+        'billing_id': odoo_partner_contact.id,
+        'shipping_id': odoo_partner_delivery.id
+    }
 
+
+def save_quotation(ep_client, ids):
+    order = ep_client.model('sale.order')
+    
+    odoo_order_data = {
+        'partner_id': ids['billing_id'],
+        'partner_shipping_id': ids['shipping_id'],
+        'state': 'sent',
+        'incoterm': 15, # delivered duty paid
+        'payment_term': 1 #immediate payment
+    }
+    
+    odoo_order = order.browse(168)
+    update_odoo_object(odoo_order, **odoo_order_data)
+    #order.create(odoo_order)
+    
+    
+    order_line = ep_client.model('sale.order.line')
+    odoo_order_line_data = {
+        'order_id': 168,
+        'order_partner_id': ids['billing_id'],
+        'product_uos_qty': 1,
+        'product_uom': 1,
+        'product_id': 4,
+        'state': 'draft'
+    }
+    
+    odoo_order_line = order_line.browse(173)
+    update_odoo_object(odoo_order_line, **odoo_order_line_data)
+    #order_line.create(odoo_order_line)
+    
 
 def test():
     erp_client = get_erp_client()
+    
     customer = {
-        'odoo_contact_id': None,
-        'name': u"Test name",
-        'email': u"test@example.com",
-        'phone': u"911",
-        'billing_address': u"Billing address \n boejaaa",
-        'shipping_address': u"Shipping address \n boejaaa"
+        'billing': {
+            'id': 120,
+            'name': u"Test name billing",
+            'email': u"test@example.com billing",
+            'phone': u"911",
+            'address': u"Billing address \n boejaaa"
+        },
+        'shipping': {
+            'id': 121,
+            'name': u"Test name shipping",
+            'email': u"test@example.com shipping",
+            'phone': u"112",
+            'address': u"Shipping address \n boejaaa"
+        }
     }
-    create_quotation(erp_client, customer)
+    
+    ids = save_customer(erp_client, customer)
+    save_quotation(erp_client, ids)
+    
