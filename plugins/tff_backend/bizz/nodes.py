ee4a3708ae5209eadf46bc8dcabeeefae42c6bc4
Bart Pede <bart.pede@mobicage.com>
2018-02-22 14:35:15 +0100

    Check node statuses via zero-robot (#284)
    * Getting node statuses from 0-robot (253)
    * Fixes for checking node statuses (253)
    * Refresh JWT before checking node statuses (253)
    * Create ThreeFoldWallet if needed when receiving coins (260)

--- plugins/tff_backend/bizz/nodes.py
+++ plugins/tff_backend/bizz/nodes.py
@@ -14,17 +14,18 @@
 # limitations under the License.
 #
 # @@license_version:1.3@@
+from collections import defaultdict
 import json
 import logging
-from collections import defaultdict
-
-from google.appengine.api import urlfetch, apiproxy_stub_map
-from google.appengine.ext import ndb
-from google.appengine.ext.deferred import deferred
+import time
+import types
 
 from framework.bizz.job import run_job
 from framework.plugin_loader import get_config
 from framework.utils import now
+from google.appengine.api import urlfetch, apiproxy_stub_map
+from google.appengine.ext import ndb
+from google.appengine.ext.deferred import deferred
 from mcfw.cache import cached
 from mcfw.consts import DEBUG
 from mcfw.rpc import returns, arguments
@@ -38,6 +39,8 @@ from plugins.tff_backend.bizz.messages import send_message_and_email
 from plugins.tff_backend.bizz.odoo import get_nodes_from_odoo
 from plugins.tff_backend.bizz.todo import HosterSteps, update_hoster_progress
 from plugins.tff_backend.consts.hoster import DEBUG_NODE_DATA
+from plugins.tff_backend.libs.zero_robot.EnumTaskState import EnumTaskState
+from plugins.tff_backend.libs.zero_robot.Task import Task
 from plugins.tff_backend.models.hoster import NodeOrder, NodeOrderStatus
 from plugins.tff_backend.models.user import TffProfile, NodeInfo
 from plugins.tff_backend.plugin_consts import NAMESPACE
@@ -46,8 +49,7 @@ from plugins.tff_backend.utils.app import get_app_user_tuple
 
 
 @returns(apiproxy_stub_map.UserRPC)
-def _orc_call(path):
-    # type: (basestring) -> apiproxy_stub_map.UserRPC
+def _async_zero_robot_call(path, method=urlfetch.GET, payload=None):
     cfg = get_config(NAMESPACE)
     try:
         jwt = _refresh_jwt(cfg.orchestator.jwt)
@@ -55,9 +57,10 @@ def _orc_call(path):
         msg = 'Could not refresh JWT'
         logging.exception(msg)
         raise deferred.PermanentTaskFailure(msg)
-    headers = {'Authorization': u'Bearer %s' % jwt}
+    headers = {'Authorization': u'Bearer %s' % jwt, 'Content-Type': 'application/json'}
+    url = u'https://zero-robot.threefoldtoken.com%s' % path
     rpc = urlfetch.create_rpc(deadline=30)
-    urlfetch.make_fetch_call(rpc, url=u'https://orc.threefoldtoken.com%s' % path, headers=headers)
+    urlfetch.make_fetch_call(rpc, payload=payload, method=method, url=url, headers=headers)
     return rpc
 
 
@@ -73,23 +76,58 @@ def _refresh_jwt(jwt):
     return refresh_jwt(jwt, 3600)
 
 
-def get_nodes_status(node_ids):
-    rpcs = []
-    for node_id in node_ids:
-        rpcs.append(_orc_call('/nodes/%s' % node_id))
-    results = [rpc.get_result() for rpc in rpcs]
-    for result in results:
-        if result.status_code == 404:
-            return u'not_found'
-        if result.status_code != 200:
-            if 400 <= result.status_code < 500:
-                exception_class = deferred.PermanentTaskFailure
+def _get_task_url(task):
+    return '/services/%s/task_list/%s' % (task.service_guid, task.guid)
+
+
+@returns([object])
+@arguments(tasks=[Task], callback=types.FunctionType, deadline=int)
+def _wait_for_tasks(tasks, callback=None, deadline=50):
+    results = []
+    start_time = time.time()
+    incomplete_tasks = {t.guid: t for t in tasks}
+    while incomplete_tasks:
+        rpcs = {task.guid: (task, _async_zero_robot_call(_get_task_url(task)))
+                for task in incomplete_tasks.itervalues()}
+        time.sleep(2)
+        duration = time.time() - start_time
+        logging.debug('Waiting for %s tasks for %.2f seconds', len(rpcs), duration)
+        if duration > deadline:
+            logging.info('Deadline of %s seconds exceeded!', deadline)
+            break
+
+        incomplete_by_state = defaultdict(list)
+        for task, rpc in rpcs.values():
+            node_id = task.service_name
+            result = rpc.get_result()
+            if result.status_code != 200:
+                logging.error('/task_list for node %s returned status code %s. Content:\n%s',
+                              node_id, result.status_code, result.content)
+                del incomplete_tasks[task.guid]
+                continue
+
+            task = Task(**json.loads(result.content))
+            if task.state in (EnumTaskState.ok, EnumTaskState.error):
+                del incomplete_tasks[task.guid]
+                results.append(callback(task) if callback else task)
             else:
-                exception_class = Exception
-            msg = 'get_nodes_status returned status code %s for node_id %s\nContent: %s' % (
-                result.status_code, node_id, result.content)
-            raise exception_class(msg)
-    return [json.loads(result.content)['status'] for result in results]
+                incomplete_by_state[task.state].append(task.service_name)
+
+        for state, node_ids in incomplete_by_state.iteritems():
+            logging.debug('%s %s task(s) on the following nodes: %s', len(node_ids), state.value, ', '.join(node_ids))
+
+    return results
+
+
+def _node_status_callback(task):
+    status = 'running' if task.state == EnumTaskState.ok else 'halted'
+    return task.service_name, status
+
+
+def get_nodes_status(node_ids):
+    tasks = {task.service_name: task for task in _get_node_info_tasks() if task.service_name in node_ids}
+    statuses = dict(_wait_for_tasks(tasks, _node_status_callback))
+    return [statuses.get(node_id, u'not_found') for node_id in node_ids]
 
 
 def check_online_nodes():
@@ -145,31 +183,28 @@ def get_nodes_stats(nodes):
     # type: (list[NodeInfo]) -> list[dict]
     if DEBUG:
         return [_get_stats(DEBUG_NODE_DATA)]
-    rpcs = []
-    # Start all api calls at the same time and wait for all of them to finish afterwards
-    for node in nodes:
-        if node.status != 'halted':
-            logging.info('Getting stats for node %s', node)
-            rpcs.extend([
-                ('info', node.id, _orc_call('/nodes/%s/info' % node.id)),
-                ('stats', node.id, _orc_call('/nodes/%s/stats' % node.id)),
-            ])
+
+    tasks = _get_node_stats_tasks([node.id for node in nodes])
+
     results_per_node = {node.id: {'id': node.id,
                                   'status': node.status,
                                   'serial_number': node.serial_number,
                                   'info': None,
                                   'stats': None}
                         for node in nodes}
-    for key, node_id, rpc in rpcs:
-        result = rpc.get_result()
-        if result.status_code == 200:
-            results_per_node[node_id][key] = json.loads(result.content)
+
+    for task in _wait_for_tasks(tasks):
+        if task.state == EnumTaskState.ok:
+            results_per_node[task.service_name][task.action_name] = json.loads(task.result)
         else:
-            logging.warn('Response from orchestrator: %s %s' % (result.status_code, result.content))
-    all_stats = []
-    for stats in results_per_node.itervalues():
-        all_stats.append(_get_stats(stats))
-    return all_stats
+            logging.warn('Task %s on node %s has state ERROR:\n%s: %s\n%s',
+                         _get_task_url(task),
+                         task.service_name,
+                         task.eco and task.eco.exceptionclassname,
+                         task.eco and task.eco.errormessage,
+                         task.eco and task.eco._traceback)
+
+    return [_get_stats(stats) for stats in results_per_node.itervalues()]
 
 
 def get_nodes_for_user(app_user):
@@ -227,13 +262,53 @@ def _get_cpu_stats(data):
     return {'utilisation': sorted(total_cpu.values(), key=lambda v: v['start'])}
 
 
-def check_node_statuses():
-    result = _orc_call('/nodes').get_result()
+@returns([Task])
+@arguments(node_ids=[unicode])
+def _get_node_stats_tasks(node_ids):
+    blueprint = {
+        'content': {
+            'actions': [{
+                'template': 'github.com/zero-os/0-templates/node/0.0.1',
+                'actions': ['info', 'stats'],
+                'service': node_id
+            } for node_id in node_ids]
+        }
+    }
+    return _execute_blueprint(json.dumps(blueprint))
+
+
+@returns([Task])
+@arguments()
+def _get_node_info_tasks():
+    return _execute_blueprint("""{
+  "content": {
+    "actions": {
+      "template": "github.com/zero-os/0-templates/node/0.0.1",
+      "actions": "info"
+    }
+  }
+}""")
+
+
+def _execute_blueprint(blueprint):
+    logging.debug('Executing blueprint: %s', blueprint)
+    result = _async_zero_robot_call('/blueprints', urlfetch.POST, blueprint).get_result()
+    msg = '/blueprints returned status code %s. Content:\n%s' % (result.status_code, result.content)
     if result.status_code != 200:
-        msg = 'check_node_statuses returned status code %s\nContent: %s' % (result.status_code, result.content)
         raise deferred.PermanentTaskFailure(msg)
 
-    statuses = {r['id']: r['status'] for r in json.loads(result.content)}
+    logging.debug(msg)
+    return [Task(**d) for d in json.loads(result.content)]
+
+
+def _get_node_statuses(tasks):
+    statuses = dict(_wait_for_tasks(tasks, _node_status_callback))  # {node_id: status}
+    return statuses
+
+
+def check_node_statuses():
+    tasks = _get_node_info_tasks()
+    statuses = _get_node_statuses(tasks)
     run_job(_get_profiles_with_node, [], _check_node_status, [statuses])
 
 
@@ -249,7 +324,7 @@ def _check_node_status(tff_profile_key, statuses):
         for node in tff_profile.nodes:
             status = statuses.get(node.id)
             if not status:
-                logging.warn('Expected to find node %s in the ORC response', node.id)
+                logging.warn('Expected to find node %s in the response', node.id)
                 continue
             if node.status != status:
                 logging.info('Node %s of user %s changed from status "%s" to "%s"',
