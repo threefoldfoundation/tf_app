10c9ef63f7ed446d3e764425c12631008f6400f9
Lucas Vanhalst <lucas.vanhalst@gmail.com>
2018-03-02 10:35:41 +0100

    refactor client config, hardcode url for now

--- brandings/src/configuration.ts
+++ brandings/src/configuration.ts
@@ -1,7 +1,7 @@
-import { environment } from '../../../framework/client/environments/environment';
-
 export interface TffClientConfiguration {
   wallet_url: string;
 }
 
-export const configuration: TffClientConfiguration = environment.configuration.plugins.tff_backend;
+export const configuration: TffClientConfiguration = {
+  wallet_url: 'https://tff-backend.appspot.com/api/plugins/tff_backend/v1.0/wallet',
+};
