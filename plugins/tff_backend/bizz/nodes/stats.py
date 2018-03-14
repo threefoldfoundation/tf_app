0a281d27094791472473810a5500827aa60ed1f5
Lucas Vanhalst <lucas.vanhalst@gmail.com>
2018-03-14 16:46:49 +0100

    increase task timeout to 500s

--- plugins/tff_backend/bizz/nodes/stats.py
+++ plugins/tff_backend/bizz/nodes/stats.py
@@ -87,7 +87,7 @@ def _get_task_url(task):
 
 @returns([object])
 @arguments(tasks=[Task], callback=types.FunctionType, deadline=int)
-def _wait_for_tasks(tasks, callback=None, deadline=150):
+def _wait_for_tasks(tasks, callback=None, deadline=500):
     results = []
     start_time = time.time()
     incomplete_tasks = {t.guid: t for t in tasks}
