3d545170849c52085036970c2dad5ff4755a7751
Lucas Vanhalst <lucasvanhalst@users.noreply.github.com>
2018-03-22 13:29:49 +0100

    fix(nodes): Save nodes to separate model (#295)
    * fix(nodes): Save nodes to separate model
    
    * Change migration to preserve old data
    
    * make assign_nodes_to_user simpler

---
+++ plugins/tff_backend/migrations/_011_move_nodes.py
@@ -0,0 +1,44 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 GIG Technology NV
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# @@license_version:1.4@@
+from google.appengine.ext import ndb
+
+from plugins.tff_backend.models.nodes import Node, NodeStatusTime
+from plugins.tff_backend.models.user import TffProfile
+
+
+def migrate(dry_run=False):
+    to_put = []
+    for profile in TffProfile.query():
+        if profile.nodes:
+            for node in profile.nodes:
+                to_put.append(Node(key=Node.create_key(node.id),
+                                   serial_number=node.serial_number,
+                                   username=profile.username if profile.username != 'threefold_dummy_1' else None,
+                                   last_check=node.last_check,
+                                   statuses=[NodeStatusTime(status=node.status, date=node.status_date)]))
+    if dry_run:
+        return to_put
+    ndb.put_multi(to_put)
+
+
+def cleanup():
+    to_put = []
+    for profile in TffProfile.query():
+        if 'nodes' in profile._properties:
+            del profile._properties['nodes']
+            to_put.append(profile)
+    ndb.put_multi(to_put)
---
+++ plugins/tff_backend/models/nodes.py
@@ -0,0 +1,63 @@
+# -*- coding: utf-8 -*-
+# Copyright 2018 GIG Technology NV
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# @@license_version:1.4@@
+from google.appengine.ext import ndb
+
+from framework.models.common import NdbModel
+from plugins.rogerthat_api.plugin_utils import Enum
+from plugins.tff_backend.plugin_consts import NAMESPACE
+
+
+class NodeStatus(Enum):
+    HALTED = 'halted'
+    RUNNING = 'running'
+    REBOOTING = 'rebooting'
+
+
+class NodeStatusTime(NdbModel):
+    status = ndb.StringProperty(choices=NodeStatus.all())
+    date = ndb.DateTimeProperty()
+
+
+class Node(NdbModel):
+    NAMESPACE = NAMESPACE
+    serial_number = ndb.StringProperty()
+    statuses = ndb.LocalStructuredProperty(NodeStatusTime, repeated=True)  # type: list[NodeStatusTime]
+    last_check = ndb.DateTimeProperty()
+    username = ndb.StringProperty()
+    status = ndb.ComputedProperty(lambda self: self.statuses[-1].status if self.statuses else NodeStatus.HALTED)
+    status_date = ndb.DateTimeProperty()
+
+    @property
+    def id(self):
+        return self.key.string_id().decode('utf-8')
+
+    @classmethod
+    def create_key(cls, node_id):
+        # type: (unicode) -> ndb.Key
+        return ndb.Key(cls, node_id, namespace=NAMESPACE)
+
+    @classmethod
+    def list_with_user(cls):
+        return cls.query().filter(cls.username > '')  # noQA
+
+    @classmethod
+    def list_by_user(cls, username):
+        return cls.query().filter(cls.username == username)
+
+    @classmethod
+    def list_by_status(cls, status):
+        return cls.query().filter(cls.status == status)
